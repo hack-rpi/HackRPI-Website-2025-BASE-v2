c104c50c8c0c926131fdeec6d46ea7151e58e37c
DoggieGreg <DoggieGreg@users.noreply.github.com>
2025-02-28 21:45:41 +0000

    Prettified Code!

--- components/themed-components/registration-link.tsx
+++ components/themed-components/registration-link.tsx
@@ -1,9 +1,12 @@
 import HackRPILink from "./hackrpi-link";
 
-
 export default function RegistrationLink({ className }: { className?: string }) {
 	return (
-		<HackRPILink href="https://hackrpi2024.devpost.com/project-gallery" className={`${className} pl-2 pr-5 py-2` } target="_blank">
+		<HackRPILink
+			href="https://hackrpi2024.devpost.com/project-gallery"
+			className={`${className} pl-2 pr-5 py-2`}
+			target="_blank"
+		>
 			See Winners
 		</HackRPILink>
 	);
