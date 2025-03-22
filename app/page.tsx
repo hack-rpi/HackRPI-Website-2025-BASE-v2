f478454603a12f92e8139d84e4423189409dfba5
BastedEggsRYummy <baimej@rpi.edu>
2025-03-21 20:42:21 -0400

    Footer fix

--- app/page.tsx
+++ app/page.tsx
@@ -96,7 +96,6 @@ export default function Home() {
 						top: lineEnd - 20 + "px",
 					}}
 				></div>
-				<Footer />
 			</div>
 		</>
 	);
--- components/layout/client-layout.tsx
+++ components/layout/client-layout.tsx
@@ -8,6 +8,7 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
 		<>
 			<NavBar showOnScroll={true} />
 			<main>{children}</main>
+      <Footer />
 		</>
 	);
 }
